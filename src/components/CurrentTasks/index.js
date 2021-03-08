a359c2c942a0554a3d8f81b65df1ffddbe63e22d
Caleb McQuaid <calebmcquaid@gmail.com>
2021-03-08 17:08:57 -0500

    display current tasks

--- src/components/CurrentTasks/index.js
+++ src/components/CurrentTasks/index.js
@@ -3,16 +3,14 @@ const path = require('path')
 const process = require('process')
 const argv = require('yargs/yargs')(process.argv.slice(2)).argv
 
-const currentTasks = fs.readFileSync(path.resolve(__dirname, "../../shared/tasks.txt"), 'utf8', (err, data) => {
-    if(err) {
-        console.error(err)
-        return
-    }
-    console.log(data)
-})
-
 function displayCurrentTasks() {
-    console.log(currentTasks)
+    return fs.readFileSync(path.resolve(__dirname, "../../shared/tasks.txt"), 'utf8', (err, data) => {
+        if(err) {
+            console.error(err)
+            return
+        }
+        console.log(data)
+    })
 }
 
 module.exports = {
