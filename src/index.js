993540491988796db894b4ad067e51844a6f7fda
Caleb McQuaid <calebmcquaid@gmail.com>
2021-03-10 16:47:54 -0500

    Refactor menu

--- src/index.js
+++ src/index.js
@@ -1,46 +1,10 @@
 #!/usr/bin/env node
 const process = require('process')
 const { displayCurrentTasks } = require('./components/CurrentTasks');
-const ESCAPE_KEY = 27
 const argv = require('yargs/yargs')(process.argv.slice(2)).argv;
-const fs = require('fs')
-const path = require('path')
 const {addTask} = require('./components/AddTask');
 const { writeTaskTextFile } = require('./utilities/WriteFile');
-const { readTaskTextFile } = require('./utilities/ReadFile');
 const { completeTask } = require('./components/CompleteTask')
-
-
-
-// const { stdin, stdout } = process;
-// const rl = readline.createInterface({ input: stdin, output: stdout });
-
-function openApplication() {
-    const greeting = "Welcome to the tasklist! Here's what you can do:\n1. Add a task\n2. See Current Tasks\n3. Complete Tasks\nPress a number to continue:"
-    return greeting
-}
-
-function navigateToMenuOption(input) {
-    if(input == 1) {
-      return "Press esc to return to the main menu\n\nWhat needs done? Press enter to add the task"  
-    } else if(input == 2) {
-        return "Press esc to return to the main menu\n\nHere are your current tasks:"
-    } else if(input == 3) {
-        return "Nice work! Which task would you like to complete?\n(Enter the corresponding number to complete that task)"
-    } else if(input == 4) {
-        return "Here's everything you've completed so far:\n"
-    } else if(input == 6) {
-        return "Where would you like to send your tasklist? (Please enter a valid email address)"
-    } else {
-    return "Not a valid menu option! (press enter)"
-    }
-}
-
-function returnToMainMenu(input) {
-    if(input == ESCAPE_KEY) {
-        return openApplication()
-    }
-}
 
 function navigation(flag) {
     if(flag === "--help") {
@@ -59,21 +23,8 @@ function navigation(flag) {
     }
 }
 
-function readerTaskTextFile() {
-    return fs.readFileSync(path.resolve(__dirname, "./shared/tasks.txt"), 'utf8', (err, data) => {
-        if(err) {
-            console.error(err)
-            return
-        }
-        return data
-    })
-}
-
 console.log(navigation(argv))
 
 module.exports = {
-    openApplication,
-    navigateToMenuOption,
-    returnToMainMenu,
     navigation
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,14 +1,10 @@
 const { 
-    navigateToMenuOption,
-    returnToMainMenu,
     navigation
 } = require('./')
 
 const {readTaskTextFile} = require('./utilities/ReadFile')
 const yargs = require('yargs/yargs')
 jest.mock('./utilities/ReadFile')
-
-
 
 describe("Menu", () => {
     test("should display a help message when no flag is passed in", () => {
@@ -28,66 +24,11 @@ describe("Menu", () => {
             type: 'string',
             default: 'bar'
         }).argv
-        const currentTaskFlag = '--current'
         const currentTasks = "task 1\n task 3\n"
 
         const screen = navigation(argv)
 
         expect(screen).toBe(currentTasks)
-    })
-
-    test("should navigate to 'complete a task' when the proper key is pressed", () => {
-        const input = 3
-        const message = "Nice work! Which task would you like to complete?\n(Enter the corresponding number to complete that task)"
-
-        const screen = navigateToMenuOption(input)
-
-        expect(screen).toBe(message)
-    })
-
-    test("should navigate to 'See Completed Tasks' when proper input is pressed", () => {
-        const input = 4
-        const message = "Here's everything you've completed so far:\n"
-
-        const screen = navigateToMenuOption(input)
-
-        expect(screen).toBe(message)
-    })
-
-    test("should navigate to 'share tasklist' when proper input is pressed", () => {
-        const input = 6
-        const expectedGreeting = "Where would you like to send your tasklist? (Please enter a valid email address)"
-
-        const screen = navigateToMenuOption(input)
-
-        expect(screen).toBe(expectedGreeting)
-    })
-    
-    test("should navigate to the 'add task screen' when add menu number is pressed", () => {
-        const addTaskBanner = "Press esc to return to the main menu\n\nWhat needs done? Press enter to add the task"
-        const input = 1
-
-        const addTaskScreen = navigateToMenuOption(input)
-
-        expect(addTaskScreen).toBe(addTaskBanner)
-    })
-
-    test("should prompt 'invalid option' when input not in the menu is pressed", () => {
-        const expectedOuput = "Not a valid menu option! (press enter)"
-        const input = Math.random()
-
-        const screen = navigateToMenuOption(input)
-
-        expect(screen).toBe(expectedOuput)
-    })
-    
-    test("should return to the main menu when the escape key is pressed", () => {
-        const escapeKey = 27
-        const mainMenuGreeting = "Welcome to the tasklist! Here's what you can do:\n1. Add a task\n2. See Current Tasks\n3. Complete Tasks\nPress a number to continue:"
-
-        const screen = returnToMainMenu(escapeKey)
-
-        expect(screen).toBe(mainMenuGreeting)
     })
 })
 
