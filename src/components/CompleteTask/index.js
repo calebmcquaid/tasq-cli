d15fefdfa4c3862eee22130fad2946bcbcc27cd3
Caleb McQuaid <calebmcquaid@gmail.com>
2021-03-10 16:49:26 -0500

    Remove console.log

--- src/components/CompleteTask/index.js
+++ src/components/CompleteTask/index.js
@@ -2,7 +2,6 @@ const { readTaskTextFile } = require('../../utilities/ReadFile/index')
 
 function completeTask(taskNumber) {
     const currentTasks = readTaskTextFile()
-    console.log(currentTasks.split('\n'))
     const task = taskNumber -1
     const splitTasks = currentTasks.split('\n')
     splitTasks.splice(task, 1)
