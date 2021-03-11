dd91b99363997c016047190927a141352a881445
Caleb McQuaid <calebmcquaid@gmail.com>
2021-03-11 15:13:09 -0500

    update help options

--- src/index.test.js
+++ src/index.test.js
@@ -9,7 +9,7 @@ jest.mock('./utilities/ReadFile')
 describe("Menu", () => {
     test("should display a help message when no flag is passed in", () => {
     // ARRANGE: (mocks[node modules, "stand in", things I don't own (api calls)], spies, expected output) welcome message - variable, 
-        const greeting = "Welcome to the tasklist! Here's what you can do:\n\nAdd a task with --add and the task: todo --add 'new task'\n\nSee Current Tasks with --current: todo --current\n\nComplete Tasks with --complete and the task number: todo --complete 1\n\nEnter 'task --help' to see a list of the commands."
+        const greeting = "Welcome to the tasklist! Here's what you can do:\n\nAdd a task with --add and the task: todo --add 'new task'\n\nSee current tasks with --current: todo --current\n\nDelete current tasks with --delete: todo --delete 1\n\nComplete tasks with --complete and the task number: todo --complete 1\n\nSee completed tasks with --completed: todo --completed\n\nEnter 'task --help' to see a list of the commands."
     // ACT:
         const screen = navigation('')
     // ASSERT:
@@ -38,7 +38,7 @@ describe('Help', () => {
             type: 'string',
             default: 'bar'
         }).help('').argv
-        const helpMenu = "  --add ' '\t\t Add a task\n  --current\t\t See current tasks\n  --complete ' '\t Complete a task with the corresponding number\n  --archive ' '\t\t Permanently delete completed task with the corresponding number"
+        const helpMenu = "  --add ' '\t\t Add a task\n  --current\t\t See current tasks\n  --delete ' '\t\t Delete a current task by entering the corresponding number\n  --complete ' '\t Complete a task with the corresponding number\n  --completed\t\t See the tasks you have completed\n  --archive ' '\t\t Permanently delete completed task with the corresponding number"
 
         const screen = navigation(argv)
 
