56f2f56ce063fe6f580c27baea9352160adf7474
Caleb McQuaid <calebmcquaid@gmail.com>
2021-03-12 14:45:48 -0500

    updated menu options

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const {updateTask} = require('./components/UpdateTask')
 
 function navigation(flag) {
     if(flag.help) {
-        return "  --add ' '\t\t Add a task\n  --current\t\t See current tasks\n  --delete ' '\t\t Delete a current task by entering the corresponding number\n  --complete ' '\t Complete a task with the corresponding number\n  --completed\t\t See the tasks you have completed\n  --archive ' '\t\t Permanently delete completed task with the corresponding number"
+        return "  --add ' '\t\t Add a task\n  --current\t\t See current tasks\n  --update # ' '\t Update a task with the corresponding number\n  --delete ' '\t\t Delete a current task by entering the corresponding number\n  --complete ' '\t Complete a task with the corresponding number\n  --completed\t\t See the tasks you have completed\n  --archive ' '\t\t Permanently delete completed task with the corresponding number"
     } else if(flag.current) {
       return displayCurrentTasks()
     } else if(flag.add) {
@@ -34,7 +34,7 @@ function navigation(flag) {
     } else if(flag.archive) {
         archiveTask(flag.archive)
     } else {
-        return "Welcome to the tasklist! Here's what you can do:\n\nAdd a task with --add and the task: tasq --add 'new task'\n\nSee current tasks with --current: tasq --current\n\nDelete current tasks with --delete: tasq --delete 1\n\nComplete tasks with --complete and the task number: tasq --complete 1\n\nSee completed tasks with --completed: tasq --completed\n\nEnter 'tasq --help' to see a list of the commands."
+        return "Welcome to the tasklist! Here's what you can do:\n\nAdd a task with --add and the task: tasq --add 'new task'\n\nUpdate a current task with --update, the task number, and the new task: tasq --update 1 'updated task'\n\nSee current tasks with --current: tasq --current\n\nDelete current tasks with --delete: tasq --delete 1\n\nComplete tasks with --complete and the task number: tasq --complete 1\n\nSee completed tasks with --completed: tasq --completed\n\nEnter 'tasq --help' to see a list of the commands."
     }
 }
 
--- src/index.test.js
+++ src/index.test.js
@@ -9,7 +9,7 @@ jest.mock('./utilities/ReadFile')
 describe("Menu", () => {
     test("should display a help message when no flag is passed in", () => {
     // ARRANGE: (mocks[node modules, "stand in", things I don't own (api calls)], spies, expected output) welcome message - variable, 
-        const greeting = "Welcome to the tasklist! Here's what you can do:\n\nAdd a task with --add and the task: tasq --add 'new task'\n\nSee current tasks with --current: tasq --current\n\nDelete current tasks with --delete: tasq --delete 1\n\nComplete tasks with --complete and the task number: tasq --complete 1\n\nSee completed tasks with --completed: tasq --completed\n\nEnter 'tasq --help' to see a list of the commands."
+        const greeting = "Welcome to the tasklist! Here's what you can do:\n\nAdd a task with --add and the task: tasq --add 'new task'\n\nUpdate a current task with --update, the task number, and the new task: tasq --update 1 'updated task'\n\nSee current tasks with --current: tasq --current\n\nDelete current tasks with --delete: tasq --delete 1\n\nComplete tasks with --complete and the task number: tasq --complete 1\n\nSee completed tasks with --completed: tasq --completed\n\nEnter 'tasq --help' to see a list of the commands."
     // ACT:
         const screen = navigation('')
     // ASSERT:
@@ -38,7 +38,7 @@ describe('Help', () => {
             type: 'string',
             default: 'bar'
         }).help('').argv
-        const helpMenu = "  --add ' '\t\t Add a task\n  --current\t\t See current tasks\n  --delete ' '\t\t Delete a current task by entering the corresponding number\n  --complete ' '\t Complete a task with the corresponding number\n  --completed\t\t See the tasks you have completed\n  --archive ' '\t\t Permanently delete completed task with the corresponding number"
+        const helpMenu = "  --add ' '\t\t Add a task\n  --current\t\t See current tasks\n  --update # ' '\t Update a task with the corresponding number\n  --delete ' '\t\t Delete a current task by entering the corresponding number\n  --complete ' '\t Complete a task with the corresponding number\n  --completed\t\t See the tasks you have completed\n  --archive ' '\t\t Permanently delete completed task with the corresponding number"
 
         const screen = navigation(argv)
 
