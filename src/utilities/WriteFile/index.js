d39b1c19187f33210e8ccd7707cb7e41471d7430
Caleb McQuaid <calebmcquaid@gmail.com>
2021-03-26 14:46:47 -0400

    Redoing write task

--- src/utilities/WriteFile/index.js
+++ src/utilities/WriteFile/index.js
@@ -1,16 +1,14 @@
 const fs = require('fs')
 const { CURRENT_TASKS_DIRECTORY, COMPLETED_TASKS_DIRECTORY } = require('../../shared/constants');
-const {formatTask, trimNumbers} = require('../../shared/formatting');
+const {trimNumbers} = require('../../shared/formatting');
 
 
 function writeTaskTextFile(currentTasks) {
-    const trimmedNumbers = trimNumbers(currentTasks)
-    fs.writeFileSync(CURRENT_TASKS_DIRECTORY, formatTask(trimmedNumbers))
+    fs.writeFileSync(CURRENT_TASKS_DIRECTORY, currentTasks)
 }
 
 function writeCompletedTaskTextFile(completedTask) {
-    const trimmedNumbers = trimNumbers(completedTask)
-    fs.writeFileSync(COMPLETED_TASKS_DIRECTORY, formatTask(trimmedNumbers))
+    fs.writeFileSync(COMPLETED_TASKS_DIRECTORY, completedTask)
 }
 
 module.exports = {
