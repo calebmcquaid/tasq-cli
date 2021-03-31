f225ae96809b02a7b5f1d990854089a6be908f91
Caleb McQuaid <calebmcquaid@gmail.com>
2021-03-31 17:22:42 -0400

    Rework update task

--- src/components/UpdateTask/index.js
+++ src/components/UpdateTask/index.js
@@ -3,11 +3,10 @@ const { readCurrentTaskFile } = require("../../utilities/ReadFile");
 function updateTask(taskNumber, updatedTask) {
     const currentTasks = readCurrentTaskFile()
     const task = taskNumber - 1
-    const splitTasks = currentTasks.split('\n')
-    let removedTask = splitTasks.splice(task, 1)
-    removedTask = updatedTask
-    splitTasks.splice(task, 0, removedTask)
-    return splitTasks.join('\n')
+    let taskToUpdate = currentTasks.currentTasks.splice(task, 1)[0]
+    taskToUpdate.title = updatedTask
+    currentTasks.currentTasks.splice(task, 0, taskToUpdate)
+    return currentTasks
 }
 
 module.exports = {
--- src/components/UpdateTask/index.test.js
+++ src/components/UpdateTask/index.test.js
@@ -5,10 +5,30 @@ jest.mock('../../utilities/ReadFile')
 
 describe("Update task", () => {
     test("should update the task when given the proper flag", () => {
-        readCurrentTaskFile.mockImplementation(() => {return '1. Hello\n2. New task'})
+        readCurrentTaskFile.mockImplementation(() => {
+            return {
+                "currentTasks": [
+                    {
+                        "id": 1,
+                        "title": "test",
+                        "isArchived": false,
+                        "isCompleted": false
+                    },
+                ]
+            }
+        })
         const taskNumber = 1
         const newTask = "This is an updated task"
-        const updatedTasks = "This is an updated task\n2. New task"
+        const updatedTasks = {
+            "currentTasks": [
+                {
+                    "id": 1,
+                    "title": "This is an updated task",
+                    "isArchived": false,
+                    "isCompleted": false
+                },
+            ]
+        }
 
         const screen = updateTask(taskNumber, newTask)
 
